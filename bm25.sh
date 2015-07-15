6ac25ac440cd1f038105d45b7af32c6b392c16ca
Martin Matulík <matulma4@fel.cvut.cz>
2015-07-15 14:10:43 +0200

    Added logging info

--- bm25.sh
+++ bm25.sh
@@ -8,13 +8,23 @@
 log_echo() {
   echo $(date "+%Y%m%d-%H.%M.%S") " $@";
 }
-
+log_echo "Starting..."
+log_echo "Unloading..."
 module unload python34-modules-intel
+log_echo "Done."
+log_echo "Loading modules."
 module add python27-modules-gcc
+log_echo "Done."
+
+INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
 
 cd $SCRATCHDIR
 N_JOBS=1
 
-python bm25.py
-
+log_echo "Running script..."
+python $INPUT_DIR/bm25.py
+log_echo "Done."
+log_echo "Cleaning up..."
 rm -rf $SCRATCHDIR/*
+log_echo "Done."
+log_echo "Finished."
