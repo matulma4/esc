8720faa02c5c0bacd88136996ec95138ee784f4c
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-03 12:47:18 +0200

    Added testing of models

---
+++ model_tester.py
@@ -0,0 +1,18 @@
+from gensim import models
+import sys
+
+if __name__ == "__main__":
+    model = models.Word2Vec.load(sys.argv[1])
+    f = open(sys.argv[1]+'.out','w')
+    for doc in model.most_similar(positive=['krab', 'ruka'], negative=['člověk']):
+        f.write(doc[0].encode('utf-8')+" "+str(doc[1]))
+    print('--------------------------------------')
+    for doc in model.most_similar(positive=['nafta', 'člověk'], negative=['jídlo']):
+        f.write(doc[0].encode('utf-8')+" "+str(doc[1]))
+    f.write('--------------------------------------')
+    for doc in model.most_similar(positive=['silnice', 'vlak'], negative=['kolej']):
+        f.write(doc[0].encode('utf-8')+" "+str(doc[1]))
+    f.write('--------------------------------------')
+
+    f.write(model.doesnt_match("bratr sestra auto strýc".split()).encode('utf-8'))
+    f.close()
---
+++ model_tester.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -N w2vg
+#PBS -l walltime=24h
+#PBS -l nodes=2:ppn=4
+#PBS -l mem=8gb
+#PBS -l scratch=16gb
+
+
+log_echo() {
+  echo $(date "+%Y%m%d-%H.%M.%S") " $@";
+}
+log_echo "Starting..."
+source /storage/brno2/home/$LOGNAME/.profile_matulma4
+log_echo "Done."
+INPUT_DIR="/storage/brno2/home/matulma4/esc/models"
+TYPE="classic"
+
+cd $SCRATCHDIR
+N_JOBS=1
+log_echo "Running scripts..."
+for doc in $INPUT_DIR/$TYPE/*.word2vec ; do
+cp $doc $SCRATCHDIR
+cp $doc.* $SCRATCHDIR
+python model_tester.py $doc
+cp $doc.out $INPUT_DIR/$TYPE
+done
+log_echo "Done."
