63e2fc9dfcf6246388288f854045bf6162280858
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-04 22:32:16 +0200

    Batch file edited

--- batch.sh
+++ batch.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#for nm in 'classic' 'lemmatized' ; do
+for nm in 'classic' 'lemmatized' ; do
 #for iter in 1 2 3 4 5 ; do
-#for size in 25 50 100 150 200 250 300 ; do
-#for win in 1 2 3 5 10 ; do
+for size in 50 100 200 300 ; do
+for win in 3 5 10 ; do
 iter=1
-size=10
-win=5
-nm="lemmatized"
-qsub -v I=$iter,S=$size,W=$win,A=25,M=5000,H=1,N=1,G=0,NAME=$nm w2v.sh
+# size=10
+# win=5
+# nm="lemmatized"
+qsub -v I=$iter,S=$size,W=$win,A=25,M=30000,H=1,N=1,G=0,NAME=$nm w2v.sh
+done
 #done
-#done
-#done
-#done
+done
+done
