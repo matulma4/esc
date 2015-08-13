778e5eb590dbc01b55c6feb3e526c822ca72f982
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-13 09:42:51 +0200

    Added getitem and len to MySentences class

--- doc_to_vec.py
+++ doc_to_vec.py
@@ -1,11 +1,11 @@
 from gensim import models
 import numpy as np
-from sklearn.manifold import TSNE
+# from sklearn.manifold import TSNE
 # from w2vg import MySentences
-import matplotlib.pyplot as plt
-from bokeh.plotting import figure,output_file,show
+# import matplotlib.pyplot as plt
+# from bokeh.plotting import figure,output_file,show
 from basicgrad import q
-from train import train
+# from train import train
 
 
 
@@ -23,30 +23,87 @@ def average_vec(mx,model,length):
     #     #     print("Word "+vec+" not in vocabulary")
     # return vec_sum/count
 
-class MyWords:
-    def __init__(self,model):
-        self.model = model
+class MySentences:
+    def __init__(self,fname):
+        self.fname = fname
+        with open(fname) as f:
+            for i, l in enumerate(f):
+                pass
+        self.length = i+1
 
     def __iter__(self):
-        for word in self.model.vocab.keys():
-            yield model[word]
+        for line in open(self.fname):
+            yield line.split()
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, item):
+        with open(self.fname) as f:
+            for i, l in enumerate(f):
+                if i == item:
+                    return l
+
 
 def visualize(model):
-    output_file("ffs.html")
     X = [model[word][0] for word in model.vocab.keys()]
     Y = [model[word][1] for word in model.vocab.keys()]
-    # p = figure(plot_width=1280,plot_height=720)
-    # # p.scatter(X[:100],Y[:100],marker="circle")
-    # labels = [word.decode('utf-8') for word in model.vocab.keys()]
-    # for label, x, y in zip(labels[20000:20500], X[20000:20500], Y[20000:20500]):
-    #     p.text(float(x),float(y),text=[label],text_font_size="10pt")
-    # show(p)
+    segments = [[] for seg in range(15)]
+    labels = [word.decode('utf-8') for word in model.vocab.keys()]
+    for x,y,label in zip(X,Y,labels):
+        if x < 0.1:
+            if y < 0.1:
+                segments[0].append((x,y,label))
+            elif y < 0.2:
+                segments[1].append((x,y,label))
+            elif y < 0.3:
+                segments[2].append((x,y,label))
+        elif x < 0.2:
+            if y < 0.1:
+                segments[3].append((x,y,label))
+            elif y < 0.2:
+                segments[4].append((x,y,label))
+            elif y < 0.3:
+                segments[5].append((x,y,label))
+        elif x < 0.3:
+            if y < 0.1:
+                segments[6].append((x,y,label))
+            elif y < 0.2:
+                segments[7].append((x,y,label))
+            elif y < 0.3:
+                segments[8].append((x,y,label))
+        elif x < 0.4:
+            if y < 0.1:
+                segments[9].append((x,y,label))
+            elif y < 0.2:
+                segments[10].append((x,y,label))
+            elif y < 0.3:
+                segments[11].append((x,y,label))
+        elif x < 0.5:
+            if y < 0.1:
+                segments[12].append((x,y,label))
+            elif y < 0.2:
+                segments[13].append((x,y,label))
+            elif y < 0.3:
+                segments[14].append((x,y,label))
+    for i in range(15):
+        output_file("bokeh\\"+str(i)+".html")
+        p = figure(plot_width=800,plot_height=600)
+        seg = segments[i]
+        # p.scatter(X[:100],Y[:100],marker="circle")
+        # labels = [word.decode('utf-8') for word in model.vocab.keys()]
+        for x, y, label in seg:
+            p.text(float(x),float(y),text=[label],text_font_size="10pt")
+        # show(p)
+
     # plt.scatter(X, Y, alpha=0.5)
     # plt.plot(X,Y, 'k.', markersize=0.5)
-    labels = [word.decode('utf-8') for word in model.vocab.keys()]
-    for label, x, y in zip(labels, X, Y):
-        plt.annotate(label,(x, y))
-    plt.show()
+
+    #     plt.figure(i)
+    #     seg = segments[i]
+    #     for x, y,label in seg:
+    #         plt.annotate(label,(x, y))
+    # plt.show()
 
 def output_tsne(model):
     # X = [model[word] for word in model.vocab.keys()]
@@ -95,12 +152,14 @@ def load_qs(model):
     dictionary = load_doc_hashes("doc_mapper.txt")
     questions = {}
     length = len(model[model.vocab.keys()[0]])
-    docs = [line.split() for line in open("content.raw_text")]
+    # docs = [line.split() for line in open("content.raw_text")]
+    docs = MySentences("content.raw_text")
     d = []
     for doc in docs:
         d.append(average_vec(doc,model,length))
-    for line in open("base_text_features.rtData"):
-        words = line.split()
+    features = MySentences("base_text_features.rtData")
+    for words in features:
+        # words = line.split()
         relevancy = int(words[0])
         qid = int(words[1].split(':')[1])
         # query = words[:x]
@@ -129,7 +188,7 @@ def load_qs(model):
     return questions
 
 if __name__ == "__main__":
-    fname = "content.word2vec"
+    fname = "model5.word2vec"
     # fname = sys.argv[1]
     model = models.Word2Vec.load(fname)
 
