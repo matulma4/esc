b4de2cbfa9711f530b793a34e76d8537cab1ef62
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-15 20:54:39 +0200

    Program crash when no words in line are in vocabulary fixed

--- thread_average.py
+++ thread_average.py
@@ -1,28 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import threading
 from gensim import models
 import numpy as np
+import io
 
 def average(lines, result, model, dim):
     length = len(lines)
     for i in range(length):
         line = lines[i]
         vec = np.matrix([model[word] for word in line if word in model.vocab.keys()])
-        if len(vec) == 0:
-            vec = np.zeros(dim)
-        result[i] = np.mean(vec,0)
+        if not vec.any():
+            result[i] = np.zeros(dim)
+        else:
+            result[i] = np.mean(vec,0)
 
 
 
 
 if __name__ == "__main__":
     threads = []
-    n = 16
+    n = 3
     model = models.Word2Vec.load("model5.word2vec")
-    lines = [line for line in open("temp.raw_text")]
+    lines = [line.split() for line in open("temp.raw_text")]
     length = len(lines)
     dim = len(model[model.vocab.keys()[0]])
     result = np.empty(shape=(length,dim))
     size = length/(n-1)
+
     for i in range(0,length,size):
         t = threading.Thread(target=average,args=(lines[i:i+size],result[i:i+size],model,dim,))
         threads.append(t)
