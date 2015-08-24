cf4c47e58f8d40887144734d29776e6cf9cfcd07
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-24 21:12:12 +0200

    Added shell script for running extraction

---
+++ extract.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -N avg
+#PBS -l walltime=24h
+#PBS -l nodes=1:ppn=16
+#PBS -l mem=32gb
+#PBS -l scratch=16gb
+
+INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
+DATA_NAME="base_text_features.rtData"
+PATH_NAME="models/lemmatized/default"
+
+log_echo() {
+  echo $(date "+%Y%m%d-%H.%M.%S") " $@";
+}
+
+log_echo "Starting..."
+source /storage/brno2/home/$LOGNAME/.profile_matulma4
+log_echo "Done."
+
+cd $SCRATCHDIR
+N_JOBS=1
+
+cp $INPUT_DIR/$DATA_NAME
+cp $INPUT_DIR/temp_mapper.txt
+cp extractor.py $SCRATCHDIR
+cp doc_to_vec.py $SCRATCHDIR
+cp appender.py $SCRATCHDIR
+python $SCRATCHDIR/extractor.py
+cp $SCRATCHDIR/temp_features.rtData $INPUT_DIR
+rm -rf $SCRATCHDIR/*
