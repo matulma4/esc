0afba6dbcd5dbb54d5ab87d3ed95f2e2b4f11014
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-25 09:30:49 +0200

    Fixed errors during copying

--- extract.sh
+++ extract.sh
@@ -6,7 +6,7 @@
 #PBS -l scratch=16gb
 
 INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
-DATA_NAME="temp_text_features.rtData"
+DATA_NAME="temp_features.rtData"
 PATH_NAME="models/lemmatized/default"
 
 log_echo() {
@@ -21,7 +21,7 @@ cd $SCRATCHDIR
 N_JOBS=1
 
 cp $INPUT_DIR/$DATA_NAME $SCRATCHDIR
-cp $INPUT_DIR/$PATH_NAME/content.raw_text
+cp $INPUT_DIR/$PATH_NAME/content.raw_text $SCRATCHDIR
 cp $INPUT_DIR/doc_mapper.txt.gz $SCRATCHDIR
 gunzip $SCRATCHDIR/doc_mapper.txt.gz
 cp $INPUT_DIR/extractor.py $SCRATCHDIR
