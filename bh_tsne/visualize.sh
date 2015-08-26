00ac36a23814a996ee94bfe579ded46ee8158b27
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-26 09:39:04 +0200

    Increased perplexity

--- bh_tsne/visualize.sh
+++ bh_tsne/visualize.sh
@@ -2,7 +2,7 @@
 
 for model in "lemmatized" "classic" ; do
 for theta in 0.2 0.4 0.6 0.8 1 ; do
-for perplexity in 10 20 30 40 50 ; do
+for perplexity in 60 80 100 ; do
 qsub -v MODEL=$model,THETA=$theta,PERPLEXITY=$perplexity bh.sh
 done
 done
