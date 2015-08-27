81da07924e88e1a7d71f3619c1026313e1bcd595
Martin Matulík <matulma4@fel.cvut.cz>
2015-08-27 11:20:13 +0200

    Edited shell script to average big dataset

--- average.sh
+++ average.sh
@@ -2,12 +2,12 @@
 #PBS -N avg
 #PBS -l walltime=24h
 #PBS -l nodes=1:ppn=16
-#PBS -l mem=32gb
-#PBS -l scratch=16gb
+#PBS -l mem=64gb
+#PBS -l scratch=32gb
 
 INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
-MODEL_NAME="model5"
-DATA_NAME="temp_new.raw_text"
+MODEL_NAME="content"
+DATA_NAME="content.raw_text"
 PATH_NAME="models/lemmatized/default"
 
 log_echo() {
@@ -21,26 +21,26 @@ log_echo "Done."
 cd $SCRATCHDIR
 N_JOBS=1
 log_echo "Copying model file."
-if [ -f $INPUT_DIR/$MODEL_NAME.word2vec ]
+if [ -f $INPUT_DIR/$PATH_NAME/$MODEL_NAME.word2vec ]
 then
-    cp $INPUT_DIR/$MODEL_NAME.* $SCRATCHDIR
+    cp $INPUT_DIR/$PATH_NAME/$MODEL_NAME.* $SCRATCHDIR
     log_echo "Model found."
     else
     log_echo "Model not found."
     exit 1
    fi
 log_echo "Copying data."
-cp $INPUT_DIR/$DATA_NAME $SCRATCHDIR
+cp $INPUT_DIR/$PATH_NAME/$DATA_NAME $SCRATCHDIR
 log_echo "Splitting the input file"
-split -l 625 -a 2 -d $DATA_NAME document_
-mkdir chunks
-cp document_* chunks
+split -l 78400 -a 2 -d $DATA_NAME document_
+mkdir $SCRATCHDIR/chunks
+cp document_* $SCRATCHDIR/chunks
 log_echo "Done."
 cp $INPUT_DIR/thread_average.py $SCRATCHDIR
 log_echo "Done."
 python $SCRATCHDIR/thread_average.py 16 $MODEL_NAME.word2vec
 log_echo "Done."
-cp $SCRATCHDIR/*.out $INPUT_DIR
+cp $SCRATCHDIR/chunks/*.out $INPUT_DIR
 log_echo "Cleaning up..."
 rm -rf $SCRATCHDIR/*
 log_echo "Done."
