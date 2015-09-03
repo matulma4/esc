d32d90eab86e1bbfeedf0b4e9d85fe3ccd758e4a
Martin Matulík <matulma4@fel.cvut.cz>
2015-09-03 10:10:21 +0200

    Fixed dollar sign

--- batch_average.sh
+++ batch_average.sh
@@ -2,6 +2,6 @@
 
 split -l 125390 -a 2 -d models/lemmatized/default/content.raw_text document_
 for a in document_* ; do
-qsub -v $DOC_NAME=$a average.sh
+qsub -v DOC_NAME=$a average.sh
 done
 rm document_*
