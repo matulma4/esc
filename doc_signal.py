e7ad9a75a06691cc22e390ec918dea11baa2d7e9
Martin Matulík <matulma4@fel.cvut.cz>
2015-09-14 10:36:56 +0200

    Added transformation of vectors to signals

---
+++ doc_signal.py
@@ -0,0 +1,41 @@
+from doc_to_vec import parse_feature_line,load_doc_hashes,translate_hash,Doc_Model,average_vec
+import pickle,os,glob
+from gensim import models
+from scipy import io
+import numpy as np
+from joblib import Parallel,delayed
+
+def transform_to_signal(fname,queries,dictionary,M,b,model,dim,R):
+    result = []
+    lines = [line for line in open(fname)]
+    for line in lines:
+        (relevancy,qid,query,hash) = parse_feature_line(line)
+        index = translate_hash(hash,dictionary)
+        if qid in queries.keys():
+            q_vec = queries[qid]
+        else:
+            q_vec = average_vec(query,model,dim)
+            queries[qid] = q_vec
+        doc_vec = np.array([R[index]]).T
+        v = np.dot(q_vec,M)
+        e = np.dot(v,doc_vec) + b
+        # print(e)
+        result.append(e.item(0))
+    return result
+
+def transform_thread(n_processors):
+    queries = {}
+    dictionary = load_doc_hashes("temp_mapper.txt")
+    with open("doc_model.pickle") as f:
+        doc_model = pickle.load(f)
+    M = doc_model.M
+    b = doc_model.b
+    model = models.Word2Vec.load("model6.word2vec")
+    dim = len(model[model.vocab.keys()[0]])
+    R = io.mmread("R_new.mtx").T
+    files = sorted(glob.glob(os.getcwd() + "/feature_*"))
+    signals = Parallel(n_jobs=n_processors)(delayed(transform_to_signal)(fname,queries,dictionary,M,b,model,dim,R) for fname in files)
+    return signals
+
+if __name__ == "__main__":
+    transform_thread()
