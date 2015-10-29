940bfb8717581935bb5b77f0dd9be7808e7c0543
Martin Matulík <matulma4@fel.cvut.cz>
2015-10-29 21:31:37 +0100

    Parametrized file

--- practice/rank-py.py
+++ practice/rank-py.py
@@ -3,11 +3,16 @@
 
 import numpy as np
 
-import logging
+import logging,argparse
 
 from rankpy.queries import Queries
 from rankpy.models import LambdaMART
 from rankpy.metrics import *
+
+parser = argparse.ArgumentParser(description="Rank py.")
+parser.add_argument("metric",help="metric",type=int)
+parser.add_argument("iter",help="iterations",type=int)
+args = parser.parse_args()
 
 # Turn on logging.
 logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.INFO)
@@ -37,15 +42,16 @@ logging.info('Valid queries: %s' % valid_queries)
 logging.info('Test queries: %s' %test_queries)
 
 logging.info('================================================================================')
-
+metrics = {}
 # Prepare metric for this set of queries.
-# metric = NormalizedDiscountedCumulativeGain(10, queries=[train_queries, valid_queries, test_queries])
-# metric = SeznamRank(10, queries=[train_queries, valid_queries, test_queries])
-# metric = DiscountedCumulativeGain(10, queries=[train_queries, valid_queries, test_queries])
-metric = WinnerTakesAll(10, queries=[train_queries, valid_queries, test_queries])
-# metric = ExpectedReciprocalRank(10, queries=[train_queries, valid_queries, test_queries])
+metrics[0] = NormalizedDiscountedCumulativeGain(10, queries=[train_queries, valid_queries, test_queries])
+# metrics[1] = SeznamRank(10, queries=[train_queries, valid_queries, test_queries])
+metrics[1] = DiscountedCumulativeGain(10, queries=[train_queries, valid_queries, test_queries])
+metrics[2] = WinnerTakesAll(10, queries=[train_queries, valid_queries, test_queries])
+# metrics[4] = ExpectedReciprocalRank(10, queries=[train_queries, valid_queries, test_queries])
 # Initialize LambdaMART model and train it.
-model = LambdaMART(n_estimators=50000, max_depth=4, shrinkage=0.1, estopping=100, n_jobs=-1)
+model = LambdaMART(n_estimators=50000, max_depth=4, shrinkage=0.1, estopping=args.iter, n_jobs=-1)
+metric = metrics(args.metric)
 model.fit(metric, train_queries, validation=valid_queries)
 
 logging.info('================================================================================')
