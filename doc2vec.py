ac158a95574ab327aafc87e85ba233d635cdc704
Martin Matulík <matulma4@fel.cvut.cz>
2015-11-04 21:37:13 +0100

    Added file for training doc2vecs and corresponding script

---
+++ doc2vec.py
@@ -0,0 +1,19 @@
+from gensim import models
+from gensim.models.doc2vec import TaggedLineDocument
+from os import path
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train doc2vec.")
+    parser.add_argument("fname",help="filename",type=str)
+    parser.add_argument("model",help="modelname",type=str)
+    args = parser.parse_args()
+    sentences = TaggedLineDocument(args.fname)
+    outname = args.model+".doc2vec"
+    if path.isfile(outname):
+        model = models.Doc2Vec.load(outname)
+    else:
+        model = models.Doc2Vec(size=100, window=5, min_count=5,workers=4)
+        model.build_vocab(sentences)
+        model.train(sentences)
+        model.save(outname)
---
+++ doc2vec.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#PBS -N doc2vec
+#PBS -l walltime=24h
+#PBS -l nodes=1:ppn=4
+#PBS -l mem=8gb
+#PBS -l scratch=16gb
+
+log_echo() {
+  echo $(date "+%Y%m%d-%H.%M.%S") " $@";
+}
+
+log_echo "Starting..."
+source /storage/brno2/home/$LOGNAME/.profile_matulma4
+log_echo "Done."
+INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
+FNAME="content.raw_text"
+PATH="models/lemmatized/default"
+MODEL="model1"
+cd $SCRATCHDIR
+N_JOBS=1
+log_echo "Copying files..."
+cp $INPUT_DIR/$PATH/$fname %SCRATCHDIR
+cp $INPUT_DIR/doc2vec.py $SCRATCHDIR
+log_echo "Done"
+log_echo "Running script..."
+python $SCRATCHDIR/doc2vec.py $FNAME $MODEL
+log_echo "Done"
+log_echo "Copying result..."
+cp $SCRATCHDIR/*.doc2vec $INPUT_DIR
+log_echo "Finished"
