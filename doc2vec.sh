80b21468468af88006e18d4105ad434147f6b26f
Martin Matulík <matulma4@fel.cvut.cz>
2015-11-05 23:38:42 +0100

    Fixed variable name

--- doc2vec.sh
+++ doc2vec.sh
@@ -15,12 +15,12 @@ source /storage/brno2/home/$LOGNAME/.profile_matulma4
 log_echo "Done."
 INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
 FNAME="content.raw_text"
-PATH="models/lemmatized/default"
+PTH="models/lemmatized/default"
 MODEL="model1"
 cd $SCRATCHDIR
 N_JOBS=1
 log_echo "Copying files..."
-cp $INPUT_DIR/$PATH/$fname $SCRATCHDIR
+cp $INPUT_DIR/$PTH/$fname $SCRATCHDIR
 cp $INPUT_DIR/doc2vec.py $SCRATCHDIR
 log_echo "Done"
 log_echo "Running script..."
