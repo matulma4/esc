995f439c09cbfd740464b8289826d2e056a07a0b
Martin Matulík <matulma4@fel.cvut.cz>
2015-11-14 22:08:20 +0100

    Added scripts for training ordinal regression

---
+++ logistic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -N ordinal
+#PBS -l walltime=24h
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=128gb
+#PBS -l scratch=16gb
+
+log_echo() {
+  echo $(date "+%Y%m%d-%H.%M.%S") " $@";
+}
+
+log_echo "Starting..."
+source /storage/brno2/home/$LOGNAME/.profile_matulma4
+log_echo "Done."
+INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
+FNAME="rel4.txt"
+MODEL="model2.doc2vec"
+cd $SCRATCHDIR
+N_JOBS=1
+log_echo "Copying files..."
+cp $INPUT_DIR/$FNAME $SCRATCHDIR
+cp $INPUT_DIR/ord_regression.py $SCRATCHDIR
+cp $INPUT_DIR/logistic.py $SCRATCHDIR
+log_echo "Done"
+log_echo "Running script..."
+python $SCRATCHDIR/ord_regression.py $MODEL $FNAME
+log_echo "Done"
+log_echo "Copying result..."
+cp $SCRATCHDIR/*.pickle $INPUT_DIR
+log_echo "Finished"
---
+++ ord_regression.py
@@ -0,0 +1,38 @@
+from logistic import ordinal_logistic_fit
+import numpy as np
+import argparse,pickle
+from gensim import models
+def compute_confmat(X,w):
+    Y = []
+    w = np.matrix(w)
+    for x in X:
+        Y.append((w*np.transpose(np.matrix(x)))[0,0])
+    confmat = np.zeros((6,6))
+    for i in range(len(y)):
+        predict = int(float(Y[i])+0.5)
+        label = y[i]
+        confmat[predict][label] += 1
+    print confmat
+
+def features(fname):
+    lines = [line for line in open(fname)]
+    X = []
+    y = []
+    for l in lines:
+        data = l.split('#')[0].split()
+        y.append(int(data[0]))
+        X.append([float(d.split(':')[1]) for d in data[3:]])
+    return (X,y)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compute ordinal logistic regression")
+    parser.add_argument("fname",help="modelname",type=str)
+    parser.add_argument("rel",help="relname",type=str)
+    args = parser.parse_args()
+    y = [int(line) for line in open(args.relname)]
+    model = models.Doc2Vec.load(args.fname)
+    X = model.docvecs
+    (w,theta) = ordinal_logistic_fit(X,y,max_iter=1000)
+    with open("regress.pickle","wb") as f:
+        pickle.dump((w,theta))
+
+
