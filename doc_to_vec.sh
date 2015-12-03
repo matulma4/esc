dfcbe4ada8bbfe50a90f905fc85934c0313a17c7
Martin Matulík <matulma4@fel.cvut.cz>
2015-12-03 14:40:01 +0100

    Changed method of loading questions

--- doc_to_vec.sh
+++ doc_to_vec.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #PBS -N doc2vec
 #PBS -l walltime=24h
-#PBS -l nodes=1:ppn=16
-#PBS -l mem=512gb
+#PBS -l nodes=1:ppn=11
+#PBS -l mem=64gb
 #PBS -l scratch=16gb
 
 PATH_NAME="models/lemmatized/default"
 INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
 MODEL_NAME="model1"
-DATA_NAME="content.raw_text"
-FTR_NAME="base_text_features.rtData"
-MAP_NAME="doc_mapper.txt.gz"
-AVG_NAME="R_new.mtx"
+DATA_NAME="rel4.txt"
+FTR_NAME="hashes2.txt"
+MAP_NAME="doc_mapper.txt"
+AVG_NAME="metadata2.txt"
 log_echo() {
   echo $(date "+%Y%m%d-%H.%M.%S") " $@";
 }
@@ -38,9 +38,9 @@ cp $INPUT_DIR/$AVG_NAME $SCRATCHDIR
 cp $INPUT_DIR/basicgrad.py $SCRATCHDIR
 cp $INPUT_DIR/train.py $SCRATCHDIR
 # cp $INPUT_DIR/newest_questions_content.pickle $SCRATCHDIR
-gunzip $SCRATCHDIR/$MAP_NAME
+#gunzip $SCRATCHDIR/$MAP_NAME
 cp $INPUT_DIR/doc_to_vec.py $SCRATCHDIR
-split -l 41480 -a 2 -d $SCRATCHDIR/$FTR_NAME $SCRATCHDIR/feature_
+#split -l 41480 -a 2 -d $SCRATCHDIR/$FTR_NAME $SCRATCHDIR/feature_
 log_echo "Done."
 python $SCRATCHDIR/doc_to_vec.py
 cp $SCRATCHDIR/*.pickle $INPUT_DIR
