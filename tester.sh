dca41a15c20468063790a9edf97fb716d8f2e8a5
Martin Matulík <matulma4@fel.cvut.cz>
2015-12-07 13:14:28 +0100

    Fixed output file name

--- tester.sh
+++ tester.sh
@@ -24,15 +24,15 @@ cp $INPUT_DIR/rank-py.py $SCRATCHDIR
 cp $INPUT_DIR/feature_converter.py $SCRATCHDIR
 NAME=$FTR_NAME
 # for i in 783 774 775 925 781 773 780 778 779 782 777 776; do
- python $SCRATCHDIR/feature_converter.py $FTR_NAME $SCRATCHDIR/$NUMBER-$NAME $NUMBER
-split -l 442239 -a 2 -d $FTR_NAME document_
+python $SCRATCHDIR/feature_converter.py $FTR_NAME $SCRATCHDIR/$NUMBER-$NAME $NUMBER
+split -l 442239 -a 2 -d $NUMBER-$NAME document_
 # ls $SCRATCHDIR
 #for m in 0 1 2; do
 mv $SCRATCHDIR/document_00 $SCRATCHDIR/data/train.txt
 mv $SCRATCHDIR/document_01 $SCRATCHDIR/data/test.txt
 mv $SCRATCHDIR/document_02 $SCRATCHDIR/data/vali.txt
-python $SCRATCHDIR/rank-py.py $METRIC $ITER &> $i.txt
-NAME=$i-$NAME
+python $SCRATCHDIR/rank-py.py $METRIC $ITER &> $ITER-$METRIC.txt
+#NAME=$i-$NAME
 #done
 #done
 cp *.txt $INPUT_DIR/feature_test_output_lor
