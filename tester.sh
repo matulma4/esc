a2f1b9c9bd3cd55959ade49dfc84058d1e1b986c
Martin Matulík <matulma4@fel.cvut.cz>
2015-12-07 13:08:55 +0100

    Changed testing method

--- tester.sh
+++ tester.sh
@@ -15,7 +15,7 @@ log_echo "Starting..."
 source /storage/brno2/home/$LOGNAME/.profile_matulma4
 log_echo "Done."
 INPUT_DIR="/storage/brno2/home/$LOGNAME/esc"
-FTR_NAME="new_text_features4.rtData"
+FTR_NAME="new_text_features7.rtData"
 cd $SCRATCHDIR
 N_JOBS=1
 mkdir $SCRATCHDIR/data
@@ -23,17 +23,17 @@ cp $INPUT_DIR/$FTR_NAME $SCRATCHDIR
 cp $INPUT_DIR/rank-py.py $SCRATCHDIR
 cp $INPUT_DIR/feature_converter.py $SCRATCHDIR
 NAME=$FTR_NAME
- for i in 783 774 775 925 781 773 780 778 779 782 777 776; do
- python $SCRATCHDIR/feature_converter.py $FTR_NAME $SCRATCHDIR/$i-$NAME $i
+# for i in 783 774 775 925 781 773 780 778 779 782 777 776; do
+ python $SCRATCHDIR/feature_converter.py $FTR_NAME $SCRATCHDIR/$NUMBER-$NAME $NUMBER
 split -l 442239 -a 2 -d $FTR_NAME document_
 # ls $SCRATCHDIR
-for m in 0 1 2; do
+#for m in 0 1 2; do
 mv $SCRATCHDIR/document_00 $SCRATCHDIR/data/train.txt
 mv $SCRATCHDIR/document_01 $SCRATCHDIR/data/test.txt
 mv $SCRATCHDIR/document_02 $SCRATCHDIR/data/vali.txt
-python $SCRATCHDIR/rank-py.py $m 50 &> $i.txt
+python $SCRATCHDIR/rank-py.py $METRIC $ITER &> $i.txt
 NAME=$i-$NAME
-done
-done
+#done
+#done
 cp *.txt $INPUT_DIR/feature_test_output_lor
 log_echo "Finished"
